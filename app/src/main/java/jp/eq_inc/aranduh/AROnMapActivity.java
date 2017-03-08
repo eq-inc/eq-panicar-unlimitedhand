d9d6ad4fb88e981e62d00608f652e06de3e554e5
EQ_htatsuguchi <tatsuguchi@eq-inc.jp>
2017-03-08 11:18:04 +0900

    ランタイムパーミッションが設定されていないときの挙動を修正

--- app/src/main/java/jp/eq_inc/aranduh/AROnMapActivity.java
+++ app/src/main/java/jp/eq_inc/aranduh/AROnMapActivity.java
@@ -1,10 +1,13 @@
 package jp.eq_inc.aranduh;
 
+import android.Manifest;
 import android.app.FragmentManager;
+import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -18,12 +21,11 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 
 import jp.co.thcomp.util.LocationUpdater;
+import jp.co.thcomp.util.RuntimePermissionUtil;
 import jp.co.thcomp.util.ToastUtil;
 import jp.eq_inc.aranduh.fragment.AROnCameraFragment;
 
 public class AROnMapActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener {
-    private static final float DEFAULT_LATLNG_AREA_SIZE = 0.001f;
-
     private GoogleMap mGoogleMap;
     private LocationUpdater mLocationUpdater;
     private Location mCurrentLocation;
@@ -33,47 +35,24 @@ public class AROnMapActivity extends AppCompatActivity implements OnMapReadyCall
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ar_on_map);
 
-        FragmentManager manager = getFragmentManager();
-        MapFragment mapFragment = (MapFragment) manager.findFragmentByTag(MapFragment.class.getName());
-        if (mapFragment == null) {
-            mapFragment = MapFragment.newInstance();
-            manager.beginTransaction().add(R.id.container, mapFragment, MapFragment.class.getName()).commitAllowingStateLoss();
-        }
-        mapFragment.getMapAsync(this);
-
-        AROnCameraFragment arOnCameraFragment = (AROnCameraFragment) manager.findFragmentByTag(AROnCameraFragment.class.getName());
-        if (arOnCameraFragment == null) {
-            arOnCameraFragment = AROnCameraFragment.newInstance(View.GONE);
-            manager.beginTransaction().add(R.id.container, arOnCameraFragment, AROnCameraFragment.class.getName()).commitAllowingStateLoss();
-        }
-
-        mLocationUpdater = new LocationUpdater(this);
-        mLocationUpdater.setLocationUpdateAccuracy(Criteria.ACCURACY_FINE);
-        mLocationUpdater.setMinLocationDistance(1f);
-        mLocationUpdater.setMinLocationUpdateIntervalMs(5000);
-        mLocationUpdater.setLocationListener(this);
-        mLocationUpdater.startPollingLocation(new LocationUpdater.OnPollingStatusListener() {
-            @Override
-            public void onChangePollingStatus(LocationUpdater.LocationUpdateStatus status) {
-                switch (status) {
-                    case Init:
-                        break;
-                    case NotAllowedPermission:
-                        break;
-                    case RequestingPermission:
-                        break;
-                    case UpdatingLocation:
-                        ToastUtil.showToast(AROnMapActivity.this, "Location Updating", Toast.LENGTH_SHORT);
-                        break;
-                }
-            }
-        });
+        RuntimePermissionUtil.requestPermissions(
+                this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.CAMERA},
+                mRuntimePermissionResultListener);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mLocationUpdater.stopPollingLocation();
+        if (mLocationUpdater != null) {
+            mLocationUpdater.stopPollingLocation();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        RuntimePermissionUtil.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     @Override
@@ -123,4 +102,48 @@ public class AROnMapActivity extends AppCompatActivity implements OnMapReadyCall
             mGoogleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
         }
     }
+
+    private RuntimePermissionUtil.OnRequestPermissionsResultListener mRuntimePermissionResultListener = new RuntimePermissionUtil.OnRequestPermissionsResultListener() {
+        @Override
+        public void onRequestPermissionsResult(@NonNull String[] permissions, @NonNull int[] grantResults) {
+            boolean enableLaunch = false;
+
+            if (grantResults != null && grantResults.length > 0) {
+                for (int result : grantResults) {
+                    if (result == PackageManager.PERMISSION_GRANTED) {
+                        enableLaunch = true;
+                    } else {
+                        enableLaunch = false;
+                        break;
+                    }
+                }
+            }
+
+            if (enableLaunch) {
+                FragmentManager manager = getFragmentManager();
+                MapFragment mapFragment = (MapFragment) manager.findFragmentByTag(MapFragment.class.getName());
+                if (mapFragment == null) {
+                    mapFragment = MapFragment.newInstance();
+                    manager.beginTransaction().add(R.id.container, mapFragment, MapFragment.class.getName()).commitAllowingStateLoss();
+                }
+                mapFragment.getMapAsync(AROnMapActivity.this);
+
+                AROnCameraFragment arOnCameraFragment = (AROnCameraFragment) manager.findFragmentByTag(AROnCameraFragment.class.getName());
+                if (arOnCameraFragment == null) {
+                    arOnCameraFragment = AROnCameraFragment.newInstance(View.GONE);
+                    manager.beginTransaction().add(R.id.container, arOnCameraFragment, AROnCameraFragment.class.getName()).commitAllowingStateLoss();
+                }
+
+                mLocationUpdater = new LocationUpdater(AROnMapActivity.this);
+                mLocationUpdater.setLocationUpdateAccuracy(Criteria.ACCURACY_FINE);
+                mLocationUpdater.setMinLocationDistance(1f);
+                mLocationUpdater.setMinLocationUpdateIntervalMs(5000);
+                mLocationUpdater.setLocationListener(AROnMapActivity.this);
+                mLocationUpdater.startPollingLocation(null);
+            } else {
+                ToastUtil.showToast(AROnMapActivity.this, "cannot launch by not allowed permissions", Toast.LENGTH_LONG);
+                finish();
+            }
+        }
+    };
 }
